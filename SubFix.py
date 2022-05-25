34795cce6169320b85a54bc118960dbc6a6d5b24
Milos Kalicanin <miloskal0108997@gmail.com>
2022-05-25 03:59:05 +0200

    Added: Convert .sub to .srt feature

--- SubFix.py
+++ SubFix.py
@@ -18,28 +18,65 @@ class MainWindow(QDialog):
     TIMESTAMP_LINE_REGEX = compile("[0-9]{2}:[0-5][0-9]:[0-5][0-9],[0-9]{3} --> "\
                    "[0-9]{2}:[0-5][0-9]:[0-5][0-9],[0-9]{3}")
 
+    SUB_LINE_REGEX = compile("\{(\d+)\}\{(\d+)\}\s*(.*)(\r)?(\n)?")
+
     ARROW_SPLIT_REGEX = compile(" --> ")
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.ui = uic.loadUi("layout.ui", self)
         self.setWindowIcon(QIcon("icon.png"))
-        self.ui.browseFpsPushButton.released.connect(self.browseForSubtitle)
-        self.ui.browseCodepagePushButton.released.connect(self.browseForSubtitle)
-        self.ui.browseRewindPushButton.released.connect(self.browseForSubtitle)
+        self.ui.browseFpsPushButton.released.connect(self.browseForSrtSubtitle)
+        self.ui.browseCodepagePushButton.released.connect(self.browseForSrtSubtitle)
+        self.ui.browseRewindPushButton.released.connect(self.browseForSrtSubtitle)
+        self.ui.browseConvertToSrtPushButton.released.connect(self.browseForSubSubtitle)
         self.ui.fixFpsPushButton.released.connect(self.correctSubtitleFps)
         self.ui.translateCodepagePushButton.released.connect(self.translateCodepage)
         self.ui.rewindPushButton.released.connect(self.rewindSubtitle)
-        self.model = QStandardItemModel()
+        self.ui.convertToSrtPushButton.released.connect(self.convertToSrt)
+        self.model = QStandardItemModel() # model for .srt files
+        self.modelSub = QStandardItemModel() # model for .sub files
         self.ui.rewindSubtitlesListView.setModel(self.model)
         self.ui.fpsSubtitlesListView.setModel(self.model)
         self.ui.codepageSubtitlesListView.setModel(self.model)
+        self.ui.convertToSrtListView.setModel(self.modelSub)
         self.ui.rewindSubtitlesListView.setSelectionMode(QAbstractItemView.NoSelection)
         self.ui.fpsSubtitlesListView.setSelectionMode(QAbstractItemView.NoSelection)
         self.ui.codepageSubtitlesListView.setSelectionMode(QAbstractItemView.NoSelection)
+        self.ui.convertToSrtListView.setSelectionMode(QAbstractItemView.NoSelection)
 
     #slot
-    def browseForSubtitle(self):
+    def convertToSrt(self):
+        """
+        Converts .sub to .srt
+        """
+        encoding = self.ui.convertToSrtEncodingComboBox.currentText()
+        fps = float(self.ui.convertToSrtFpsComboBox.currentText())
+        if encoding == "Windows 1250 (Latin)":
+            enc = "cp1250"
+        else:
+            enc = "utf-8"
+        for file in self.filenamesSub:
+            with open(file, encoding=enc) as src, \
+                 open(f"{file}.srt", "w", encoding=enc) as dst:
+                for cnt, line in enumerate(src, start=1):
+                    match = self.SUB_LINE_REGEX.match(line)
+                    if match:
+                        startFps = int(match.group(1))
+                        endFps = int(match.group(2))
+                        content = match.group(3)
+                        content = content.replace("|", "\n")
+                        original = f"[{cnt}] startFps='{startFps}' endFps='{endFps}' content='{content}'"
+                        startTimestamp = frameToTimestamp(startFps, fps)
+                        endTimestamp = frameToTimestamp(endFps, fps)
+                        corrected = f"{cnt}\n{startTimestamp} --> {endTimestamp}\n{content}\n\n"
+                        dst.write(corrected)
+                    else:
+                        print(f"no match in line {cnt}: {line}")
+        self.success("Done!")
+
+    #slot
+    def browseForSrtSubtitle(self):
         if __platform__ == "Linux":
             directory = "/home"
         elif __platform__ == "Windows":
@@ -48,10 +85,27 @@ class MainWindow(QDialog):
                                              caption="Browse for subtitles",
                                              filter="Subtitle files (*.srt | *.txt)")
         self.filenames = self.filenames[0]
-        for filename in self.filenames:
-            item = QStandardItem(filename)
+        self.model.clear()
+        for file in self.filenames:
+            item = QStandardItem(file)
             item.setEditable(False)
             self.model.appendRow(item)
+    
+    #slot
+    def browseForSubSubtitle(self):
+        if __platform__ == "Linux":
+            directory = "/home"
+        elif __platform__ == "Windows":
+            directory = "C:\\\\Users"
+        self.filenamesSub = QFileDialog.getOpenFileNames(parent=self, directory=directory,
+                                             caption="Browse for subtitles",
+                                             filter="Subtitle files (*.sub | *.txt)")
+        self.filenamesSub = self.filenamesSub[0]
+        self.modelSub.clear()
+        for file in self.filenamesSub:
+            item = QStandardItem(file)
+            item.setEditable(False)
+            self.modelSub.appendRow(item)
 
 
     #slot
@@ -61,42 +115,42 @@ class MainWindow(QDialog):
         if oldCodepage == "Windows 1250 (Latin)" and\
             newCodepage == "UTF-8 (Latin)":
             # cp1250 --> utf8 latin
-            for filename in self.filenames:
-                cp1250ToUtf8(filename)
+            for file in self.filenames:
+                cp1250ToUtf8(file)
             self.success("Conversion cp1250 --> utf8(latin) done")
         elif oldCodepage == "Windows 1250 (Latin)" and\
              newCodepage == "UTF-8 (Cyrillic)":
             # cp1250 --> utf8 cyrillic
-            for filename in self.filenames:
-                cp1250ToUtf8(filename)
-                utf8Convert(filename, direction="cyr")
-                removeTags(filename, newCodepage)
+            for file in self.filenames:
+                cp1250ToUtf8(file)
+                utf8Convert(file, direction="cyr")
+                removeTags(file, newCodepage)
             self.success("Conversion cp1250 --> utf8(cyrillic) done")
         elif oldCodepage == "UTF-8 (Cyrillic)" and\
              newCodepage == "UTF-8 (Latin)":
             # utf8 cyrillic --> utf8 latin
-            for filename in self.filenames:
-                utf8Convert(filename, direction="lat")
+            for file in self.filenames:
+                utf8Convert(file, direction="lat")
             self.success("Conversion utf8(cyrillic) --> utf8(latin) done")
         elif oldCodepage == "UTF-8 (Latin)" and\
              newCodepage == "UTF-8 (Cyrillic)":
             # utf8 latin --> utf8 cyrillic
-            for filename in self.filenames:
-                utf8Convert(filename, direction="cyr")
-                removeTags(filename, newCodepage)
+            for file in self.filenames:
+                utf8Convert(file, direction="cyr")
+                removeTags(file, newCodepage)
             self.success("Conversion utf8(latin) --> utf8(cyrillic) done")
         elif oldCodepage == "UTF-8 (Latin)" and\
              newCodepage == "Windows 1250 (Latin)":
             # utf8 latin --> cp1250
-            for filename in self.filenames:
-                utf8ToCp1250(filename)
+            for file in self.filenames:
+                utf8ToCp1250(file)
             self.success("Conversion utf8(latin) --> cp1250 done")
         elif oldCodepage == "UTF-8 (Cyrillic)" and\
              newCodepage == "Windows 1250 (Latin)":
             # utf8 cyrillic --> cp1250
-            for filename in self.filenames:
-                utf8Convert(filename, direction="lat")
-                utf8ToCp1250(filename)
+            for file in self.filenames:
+                utf8Convert(file, direction="lat")
+                utf8ToCp1250(file)
             self.success("Conversion utf8(cyrillic) --> cp1250 done")
         else:
             self.failure("Conversion failed.")
@@ -114,18 +168,17 @@ class MainWindow(QDialog):
         else:
             self.failure("Invalid encoding")
             return
-        for filename in self.filenames:
-            input = filename
+        for input in self.filenames:
             output = f"{input}.out"
-            with open(output, "w", encoding=encoding) as out:
-                with open(input,encoding=encoding) as f:
-                    for line in f:
-                        match = self.TIMESTAMP_LINE_REGEX.match(line)
-                        if match:
-                            result = correctFpsInLine(match.group(), oldFps, newFps)
-                            out.write(result + "\n")
-                        else:
-                            out.write(line)
+            with open(output, "w", encoding=encoding) as out, \
+                 open(input,encoding=encoding) as f:
+                for line in f:
+                    match = self.TIMESTAMP_LINE_REGEX.match(line)
+                    if match:
+                        result = correctFpsInLine(match.group(), oldFps, newFps)
+                        out.write(result + "\n")
+                    else:
+                        out.write(line)
 
     #slot
     def rewindSubtitle(self):
@@ -138,20 +191,19 @@ class MainWindow(QDialog):
             self.failure("Invalid encoding")
             return
         delay = int(self.ui.rewindMilisecondsLineEdit.text())
-        for filename in self.filenames:
-            input = filename
+        for input in self.filenames:
             output = f"{input}.out"
-            with open(output, "w", encoding=encoding) as out:
-                with open(input, encoding=encoding) as f:
-                    for line in f:
-                        match = self.TIMESTAMP_LINE_REGEX.match(line)
-                        if match:
-                            result = rewindLine(match.group(), delay)
-                            out.write(result + "\n")
-                        else:
-                            out.write(line)
-            remove (input)
-            rename (output, input)
+            with open(output, "w", encoding=encoding) as out, \
+                 open(input, encoding=encoding) as f:
+                for line in f:
+                    match = self.TIMESTAMP_LINE_REGEX.match(line)
+                    if match:
+                        result = rewindLine(match.group(), delay)
+                        out.write(result + "\n")
+                    else:
+                        out.write(line)
+            remove(input)
+            rename(output, input)
         self.success("Done")
 
     def success(self, message):
--- utilityFunctions.py
+++ utilityFunctions.py
@@ -1,18 +1,78 @@
 from os import remove, rename
 
+
+Lat2CyrUtf8Dic = {
+    65: 1040, #A <--> A
+    66: 1041, #B <--> Б
+    67: 1062, #C <--> Ц
+    68: 1044, #D <--> Д
+    69: 1045, #E <--> Е
+    70: 1060, #F <--> Ф
+    71: 1043, #G <--> Г
+    72: 1061, #H <--> Х
+    73: 1048, #I <--> И
+    74: 1032, #J <--> Ј
+    75: 1050, #K <--> К
+    76: 1051, #L <--> Л
+    77: 1052, #M <--> М
+    78: 1053, #N <--> Н
+    79: 1054, #O <--> О
+    80: 1055, #P <--> П
+    82: 1056, #R <--> Р
+    83: 1057, #S <--> С
+    84: 1058, #T <--> Т
+    85: 1059, #U <--> У
+    86: 1042, #V <--> В
+    90: 1047, #Z <--> З
+    381: 1046, #Ž <--> Ж
+    262: 1035, #Ć <--> Ћ
+    268: 1063, #Č <--> Ч
+    352: 1064, #Š <--> Ш
+    272: 1026, #Đ <--> Ђ
+    97: 1072, #a <--> а
+    98: 1073, #b <--> б
+    99: 1094, #c <--> ц
+    100: 1076, #d <--> д
+    101: 1077, #e <--> е
+    102: 1092, #f <--> ф
+    103: 1075, #g <--> г
+    104: 1093, #h <--> х
+    105: 1080, #i <--> и
+    106: 1112, #j <--> ј
+    107: 1082, #k <--> к
+    108: 1083, #l <--> л
+    109: 1084, #m <--> м
+    110: 1085, #n <--> н
+    111: 1086, #o <--> о
+    112: 1087, #p <--> п
+    114: 1088, #r <--> р
+    115: 1089, #s <--> с
+    116: 1090, #t <--> т
+    117: 1091, #u <--> у
+    118: 1074, #v <--> в
+    122: 1079, #z <--> з
+    263: 1115, #ć <--> ћ
+    273: 1106, #đ <--> ђ
+    269: 1095, #č <--> ч
+    353: 1096, #š <--> ш
+    382: 1078  #ž <--> ж
+    }
+
+Cyr2LatUtf8Dic = {v: k for k, v in Lat2CyrUtf8Dic.items()}
+
 # Removes <и> and <б> tags from file 
 def removeTags(fileIn, enc):
     if enc == "UTF-8 (Cyrillic)":
         encoding = "utf-8"
     else:
         return
-    with open (f"{fileIn}_", "w", encoding=encoding) as g:
-        with open (f"{fileIn}", "r", encoding=encoding) as f:
-            for line in f:
-                print(line.rstrip().replace("<и>", "").\
-                                    replace("</и>", "").\
-                                    replace("<б>","").\
-                                    replace("</б>",""), file=g)
+    with open (f"{fileIn}_", "w", encoding=encoding) as g, \
+         open (f"{fileIn}", "r", encoding=encoding) as f:
+        for line in f:
+            print(line.rstrip().replace("<и>", "").\
+                                replace("</и>", "").\
+                                replace("<б>","").\
+                                replace("</б>",""), file=g)
     remove(fileIn)
     rename(f"{fileIn}_", f"{fileIn}")
 
@@ -59,12 +119,12 @@ def cp1250ToUtf8(fileIn):
     utf8 = "abvgdđežzijklljmnnjoprstufhcčdžš".encode("utf-8").decode("utf-8")
     utf8 += "ABVGDĐEŽZIJKLLJMNNJOPRSTUFHCČDŽŠ".encode("utf-8").decode("utf-8")
 
-    with open(f"{fileIn}.utf8", "w", encoding="utf-8") as g:
-        with open(fileIn, "r", errors="ignore", encoding="cp1250") as f:
-            for line in f:
-                table = line.maketrans(cp1250, utf8)
-                line = line.translate(table).rstrip()
-                print(line, file=g)
+    with open(f"{fileIn}.utf8", "w", encoding="utf-8") as g, \
+         open(fileIn, "r", errors="ignore", encoding="cp1250") as f:
+        for line in f:
+            table = line.maketrans(cp1250, utf8)
+            line = line.translate(table).rstrip()
+            print(line, file=g)
     remove(fileIn)
     rename(f"{fileIn}.utf8", fileIn)
 
@@ -77,12 +137,12 @@ def utf8ToCp1250(fileIn):
     utf8 = "abvgdđežzijklljmnnjoprstufhcčdžš".encode("utf-8").decode("utf-8")
     utf8 += "ABVGDĐEŽZIJKLLJMNNJOPRSTUFHCČDŽŠ".encode("utf-8").decode("utf-8")
 
-    with open(f"{fileIn}.cp1250", "w", encoding="cp1250") as g:
-        with open(fileIn, "r", errors="ignore", encoding="utf-8") as f:
-            for line in f:
-                table = line.maketrans(utf8, cp1250)
-                line = line.translate(table).rstrip()
-                print(line, file=g)
+    with open(f"{fileIn}.cp1250", "w", encoding="cp1250") as g, \
+         open(fileIn, "r", errors="ignore", encoding="utf-8") as f:
+        for line in f:
+            table = line.maketrans(utf8, cp1250)
+            line = line.translate(table).rstrip()
+            print(line, file=g)
     remove(fileIn)
     rename(f"{fileIn}.cp1250", fileIn)
 
@@ -95,84 +155,27 @@ def utf8Convert(fileIn, direction="cyr"):
     direction - eighter "cyr"(default) to convert from latin to cyrillic or 
                         "lat" to convert from cyrillic to latin
     """
-    encoding = "utf-8"
-    lat2cyrDic = {
-        65: 1040, #A <--> A
-        66: 1041, #B <--> Б
-        67: 1062, #C <--> Ц
-        68: 1044, #D <--> Д
-        69: 1045, #E <--> Е
-        70: 1060, #F <--> Ф
-        71: 1043, #G <--> Г
-        72: 1061, #H <--> Х
-        73: 1048, #I <--> И
-        74: 1032, #J <--> Ј
-        75: 1050, #K <--> К
-        76: 1051, #L <--> Л
-        77: 1052, #M <--> М
-        78: 1053, #N <--> Н
-        79: 1054, #O <--> О
-        80: 1055, #P <--> П
-        82: 1056, #R <--> Р
-        83: 1057, #S <--> С
-        84: 1058, #T <--> Т
-        85: 1059, #U <--> У
-        86: 1042, #V <--> В
-        90: 1047, #Z <--> З
-        381: 1046, #Ž <--> Ж
-        262: 1035, #Ć <--> Ћ
-        268: 1063, #Č <--> Ч
-        352: 1064, #Š <--> Ш
-        272: 1026, #Đ <--> Ђ
-        97: 1072, #a <--> а
-        98: 1073, #b <--> б
-        99: 1094, #c <--> ц
-        100: 1076, #d <--> д
-        101: 1077, #e <--> е
-        102: 1092, #f <--> ф
-        103: 1075, #g <--> г
-        104: 1093, #h <--> х
-        105: 1080, #i <--> и
-        106: 1112, #j <--> ј
-        107: 1082, #k <--> к
-        108: 1083, #l <--> л
-        109: 1084, #m <--> м
-        110: 1085, #n <--> н
-        111: 1086, #o <--> о
-        112: 1087, #p <--> п
-        114: 1088, #r <--> р
-        115: 1089, #s <--> с
-        116: 1090, #t <--> т
-        117: 1091, #u <--> у
-        118: 1074, #v <--> в
-        122: 1079, #z <--> з
-        263: 1115, #ć <--> ћ
-        273: 1106, #đ <--> ђ
-        269: 1095, #č <--> ч
-        353: 1096, #š <--> ш
-        382: 1078  #ž <--> ж
-        }
 
-    cyr2latDic = {v: k for k, v in lat2cyrDic.items()}
+    enc = "utf-8"
 
     # latin ------------------------> cyrillic
     if direction == "cyr":
-        with open(f"{fileIn}.cyr_", "w", encoding=encoding) as out:    
-            with open(fileIn, "r", encoding=encoding) as f:
-                for line in f:
-                    for ch in line: # convert utf-8 number to his corresponding number from dictionary
-                        w = ord(ch)
-                        v = lat2cyrDic.get(w)  
-                        if v != None: 
-                            out.write(chr(v))
-                        else:
-                            out.write(ch)
-
-        with open(f"{fileIn}.cyr", "w", encoding=encoding) as out: # fix letters џ, њ, љ 
-            with open(f"{fileIn}.cyr_", "r", encoding=encoding) as f:
-                for line in f:
-                    fixedLine = line.replace("дж", "џ").replace("Дж", "Џ").replace("ДЖ", "Џ").replace("НЈ", "Њ").replace("Нј", "Њ").replace("нј","њ").replace("ЛЈ", "Љ").replace("Лј", "Љ").replace("лј","љ")
-                    out.write(fixedLine)
+        with open(f"{fileIn}.cyr_", "w", encoding=enc) as out, \
+             open(fileIn, "r", encoding=enc) as f:
+            for line in f:
+                for ch in line: # convert utf-8 number to his corresponding number from dictionary
+                    w = ord(ch)
+                    v = Lat2CyrUtf8Dic.get(w)  
+                    if v != None: 
+                        out.write(chr(v))
+                    else:
+                        out.write(ch)
+        # fix letters џ, њ, љ
+        with open(f"{fileIn}.cyr", "w", encoding=enc) as out, \
+             open(f"{fileIn}.cyr_", "r", encoding=enc) as f:
+            for line in f:
+                fixedLine = line.replace("дж", "џ").replace("Дж", "Џ").replace("ДЖ", "Џ").replace("НЈ", "Њ").replace("Нј", "Њ").replace("нј","њ").replace("ЛЈ", "Љ").replace("Лј", "Љ").replace("лј","љ")
+                out.write(fixedLine)
         
         remove(fileIn)
         remove(f"{fileIn}.cyr_")
@@ -181,35 +184,35 @@ def utf8Convert(fileIn, direction="cyr"):
                     
     # cyrillic ------------------------> latin
     elif direction == "lat":
-        with open(f"{fileIn}.lat", "w", encoding=encoding) as out:    
-            with open(fileIn, "r", encoding=encoding) as f:
-                for line in f:
-                    # convert utf-8 number to his corresponding 
-                    # number in dictionary
-                    for ch in line: 
-                        w = ord(ch)
-                        v = cyr2latDic.get(w)  
-                        if v != None: 
-                            out.write(chr(v))
-                        elif w == 1039: # Џ --> Dž 
-                            out.write("Dž")
-                        elif w == 1119: # џ --> dž
-                            out.write("dž")
-                        elif w == 1033: # Љ --> Lj
-                            out.write("Lj")
-                        elif w == 1113: # љ --> lj
-                            out.write("lj")
-                        elif w == 1034: # Њ --> Nj
-                            out.write("Nj")
-                        elif w == 1114: # њ --> nj
-                            out.write("nj")
-                        else:
-                            out.write(ch)
+        with open(f"{fileIn}.lat", "w", encoding=enc) as out, \
+             open(fileIn, "r", encoding=enc) as f:
+            for line in f:
+                # convert utf-8 number to his corresponding 
+                # number in dictionary
+                for ch in line: 
+                    w = ord(ch)
+                    v = Cyr2LatUtf8Dic.get(w)  
+                    if v != None: 
+                        out.write(chr(v))
+                    elif w == 1039: # Џ --> Dž 
+                        out.write("Dž")
+                    elif w == 1119: # џ --> dž
+                        out.write("dž")
+                    elif w == 1033: # Љ --> Lj
+                        out.write("Lj")
+                    elif w == 1113: # љ --> lj
+                        out.write("lj")
+                    elif w == 1034: # Њ --> Nj
+                        out.write("Nj")
+                    elif w == 1114: # њ --> nj
+                        out.write("nj")
+                    else:
+                        out.write(ch)
         remove(fileIn)
         rename(f"{fileIn}.lat", fileIn)
 
 
-def rewindLine(line,delay):
+def rewindLine(line, delay):
     timestamp1, timestamp2 = line.split(" --> ")
     ms1 = timestampToMiliseconds(timestamp1)
     ms2 = timestampToMiliseconds(timestamp2)
@@ -220,7 +223,7 @@ def rewindLine(line,delay):
     result = f"{timestamp1} --> {timestamp2}"
     return result
 
-def correctFpsInLine(line,oldFps,newFps):
+def correctFpsInLine(line, oldFps, newFps):
     timestamp1, timestamp2 = line.split(" --> ")
     frame1 = timestampToFrame(timestamp1, oldFps)
     timestamp1 = frameToTimestamp(frame1, newFps)
